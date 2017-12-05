42e66eaebcd8439e9893234bed5b1dc171cd7678
svenkovacic7 <sven.kovacic.7@gmail.com>
2017-12-05 18:31:34 +0100

    Update skripta.php

--- smtp/skripta.php
+++ smtp/skripta.php
@@ -2,8 +2,14 @@
     $email = $_POST["mail"];
     $naslov = $_POST["naslov"];
     $teskst = $_POST["poruka"];
+    
 
-    echo "Primatelj: $email</br>";
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)) { //Trazio sam na internetu i ovo je bolji nacin za provjeru email adrese, regex mi nije funkcionirao
+     echo "Primatelj: $email</br>";
+    }
+    else{
+        echo "Neispravna email adresa";
+    }
     echo "Naslov: $naslov</br>";
     echo "Tekst poruke: $tekst</br>";
 
